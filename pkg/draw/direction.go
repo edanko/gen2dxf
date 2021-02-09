915c2e010d9b81b217e0d7c80822449d6c24480d
Egor Danko <qwertyone@gmail.com>
2021-02-09 10:52:48 +1000

    mirrored based direction sign draw

--- pkg/draw/direction.go
+++ pkg/draw/direction.go
@@ -28,51 +28,99 @@ func addDirection(d *drawing.Drawing, p *gen.PartData) {
 			right = v.String
 		}
 	}
-	drawDirection(d, top, right, p.PartCogU, p.PartCogV)
+
+	var mirr bool
+
+	if p.Mirrored == 1 {
+		mirr = true
+	}
+
+	drawDirection(d, p.PartCogU, p.PartCogV, top, right, mirr)
 }
 
-func drawDirection(d *drawing.Drawing, top, right string, x, y float64) {
+func drawDirection(d *drawing.Drawing, x, y float64, top, right string, mirr bool) {
+
+	l1s := []float64{x + 0.0000, y + 90.9180, 0.0000}
+	l1e := []float64{x - 13.6377, y + 77.2803, 0.0000}
+
+	l2s := []float64{x + 0.0000, y + 90.9180, 0.0000}
+	l2e := []float64{x + 13.6377, y + 77.2803, 0.0000}
+
+	l3s := []float64{x + 0.0000, y + 13.6377, 0.0000}
+	l3e := []float64{x + 0.0000, y + 90.9180, 0.0000}
+
+	var cc, ca, l4s, l4e, l5s, l5e, l6s, l6e, tCoord []float64
+	if mirr {
+		cc = []float64{x - 13.6377, y + 13.6377, 0.0000}
+		ca = []float64{270, 0}
+
+		l4s = []float64{x - 13.6377, y + 0.0000, 0.0000}
+		l4e = []float64{x - 90.9180, y + 0.0000, 0.0000}
+
+		l5s = []float64{x - 77.2803, y + 13.6377, 0.0000}
+		l5e = []float64{x - 90.9180, y + 0.0000, 0.0000}
+
+		l6s = []float64{x - 77.2803, y - 13.6377, 0.0000}
+		l6e = []float64{x - 90.9180, y + 0.0000, 0.0000}
+
+		tCoord = []float64{x - 105, y + 12.1224, 0.0000}
+	} else {
+		cc = []float64{x + 13.6377, y + 13.6377, 0.0000}
+		ca = []float64{180, 270}
+
+		l4s = []float64{x + 13.6377, y + 0.0000, 0.0000}
+		l4e = []float64{x + 90.9180, y + 0.0000, 0.0000}
+
+		l5s = []float64{x + 77.2803, y + 13.6377, 0.0000}
+		l5e = []float64{x + 90.9180, y + 0.0000, 0.0000}
+
+		l6s = []float64{x + 77.2803, y - 13.6377, 0.0000}
+		l6e = []float64{x + 90.9180, y + 0.0000, 0.0000}
+
+		tCoord = []float64{x + 40.9131, y + 12.1224, 0.0000}
+	}
+
 	l := entity.NewLine()
-	l.Start = []float64{x + 0.0000, y + 90.9180, 0.0000}
-	l.End = []float64{x - 13.6377, y + 77.2803, 0.0000}
+	l.Start = l1s
+	l.End = l1e
 	l.SetColor(DirectionMarkingColor)
 	d.AddEntity(l)
 
 	l = entity.NewLine()
-	l.Start = []float64{x + 0.0000, y + 90.9180, 0.0000}
-	l.End = []float64{x + 13.6377, y + 77.2803, 0.0000}
+	l.Start = l2s
+	l.End = l2e
 	l.SetColor(DirectionMarkingColor)
 	d.AddEntity(l)
 
 	l = entity.NewLine()
-	l.Start = []float64{x + 0.0000, y + 13.6377, 0.0000}
-	l.End = []float64{x + 0.0000, y + 90.9180, 0.0000}
+	l.Start = l3s
+	l.End = l3e
 	l.SetColor(DirectionMarkingColor)
 	d.AddEntity(l)
 
 	c := entity.NewCircle()
-	c.Center = []float64{x + 13.6377, y + 13.6377, 0.0000}
+	c.Center = cc
 	c.Radius = 13.6377
 	a := entity.NewArc(c)
-	a.Angle = []float64{180, 270}
+	a.Angle = ca
 	a.SetColor(DirectionMarkingColor)
 	d.AddEntity(a)
 
 	l = entity.NewLine()
-	l.Start = []float64{x + 13.6377, y + 0.0000, 0.0000}
-	l.End = []float64{x + 90.9180, y + 0.0000, 0.0000}
+	l.Start = l4s
+	l.End = l4e
 	l.SetColor(DirectionMarkingColor)
 	d.AddEntity(l)
 
 	l = entity.NewLine()
-	l.Start = []float64{x + 77.2803, y + 13.6377, 0.0000}
-	l.End = []float64{x + 90.9180, y + 0.0000, 0.0000}
+	l.Start = l5s
+	l.End = l5e
 	l.SetColor(DirectionMarkingColor)
 	d.AddEntity(l)
 
 	l = entity.NewLine()
-	l.Start = []float64{x + 77.2803, y - 13.6377, 0.0000}
-	l.End = []float64{x + 90.9180, y + 0.0000, 0.0000}
+	l.Start = l6s
+	l.End = l6e
 	l.SetColor(DirectionMarkingColor)
 	d.AddEntity(l)
 
@@ -88,7 +136,7 @@ func drawDirection(d *drawing.Drawing, top, right string, x, y float64) {
 	t.Value = right
 	t.Height = 22
 	t.Rotation = 0
-	t.Coord1 = []float64{x + 40.9131, y + 12.1224, 0.0000}
+	t.Coord1 = tCoord
 	t.SetColor(DirectionTextColor)
 	d.AddEntity(t)
 }
