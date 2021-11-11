404f6eab2a5fee99c1f221380bc1bf747a7dc86c
Egor Danko <qwertyone@gmail.com>
2021-11-11 11:53:17 +1000

    more readable attrs

--- pkg/draw/attr.go
+++ pkg/draw/attr.go
@@ -1,30 +1,24 @@
 package draw
 
 import (
-	"time"
-
 	"github.com/edanko/dxf/drawing"
 )
 
 func addNestixAttrs(d *drawing.Drawing, quality, quantity, thickness, order, section, partid, ina string) {
-	t, _ := d.Text("[M]"+quality, 0, 0, 0, NestixAttrTextHeight)
-	t.SetColor(AttrColor)
-	t, _ = d.Text("[Q]"+quantity, 0, -AttrSpacing, 0, NestixAttrTextHeight)
-	t.SetColor(AttrColor)
-	t, _ = d.Text("[T]"+thickness, 0, -2*AttrSpacing, 0, NestixAttrTextHeight)
-	t.SetColor(AttrColor)
-	t, _ = d.Text("[B]"+order, 0, -3*AttrSpacing, 0, NestixAttrTextHeight)
-	t.SetColor(AttrColor)
-	t, _ = d.Text("[S]"+section, 0, -4*AttrSpacing, 0, NestixAttrTextHeight)
-	t.SetColor(AttrColor)
-	t, _ = d.Text("[P]"+partid, 0, -5*AttrSpacing, 0, NestixAttrTextHeight)
-	t.SetColor(AttrColor)
-	t, _ = d.Text("[I]"+ina, 0, -6*AttrSpacing, 0, NestixAttrTextHeight)
-	t.SetColor(AttrColor)
-	t, _ = d.Text("[N]"+ina, 0, -7*AttrSpacing, 0, NestixAttrTextHeight)
-	t.SetColor(AttrColor)
-	t, _ = d.Text("[A]"+ina, 0, -8*AttrSpacing, 0, NestixAttrTextHeight)
-	t.SetColor(AttrColor)
-	t, _ = d.Text("[O]"+time.Now().Format("02.01.06"), 0, -9*AttrSpacing, 0, NestixAttrTextHeight)
-	t.SetColor(AttrColor)
+	as := []string{
+		"[M]" + quality,
+		"[Q]" + quantity,
+		"[T]" + thickness,
+		"[B]" + order,
+		"[S]" + section,
+		"[P]" + partid,
+		"[I]" + ina,
+		"[N]" + ina,
+		"[A]" + ina,
+	}
+
+	for i, a := range as {
+		t, _ := d.Text(a, 0, -float64(i)*AttrSpacing, 0, NestixAttrTextHeight)
+		t.SetColor(AttrColor)
+	}
 }
