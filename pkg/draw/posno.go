9bed95bd2533a4ab4f040e6a507d5b75ce464820
Egor Danko <qwertyone@gmail.com>
2021-11-26 12:58:31 +1000

    direction drawing useless because in gen-files it's not always points to the right direction

--- pkg/draw/posno.go
+++ pkg/draw/posno.go
@@ -15,19 +15,9 @@ func addPosNo(d *drawing.Drawing, p *gen.PartData) {
 		id += "(m)"
 	}
 
-	var x, y float64
-
-	if p.PartArea < MinPartAreaForDirectionMark {
-		x = p.PartCogU
-		y = p.PartCogV
-	} else {
-		x = p.PartCogU
-		y = p.PartCogV - 15
-	}
-
 	t := entity.NewText()
 	t.Value = id
-	t.Coord1 = []float64{x, y, 0}
+	t.Coord1 = []float64{p.PartCogU, p.PartCogV, 0}
 	t.Height = float64(PosNoTextHeight)
 	t.SetColor(color.Red)
 	t.Anchor(entity.CENTER_TOP)
